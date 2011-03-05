60078fafac4f452d4aa1702f7c03f969022fb81d
Jarad Downing <jaraddowning@gmail.com>
2011-03-05 15:54:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esra::Application.config.secret_token = '9d595dc2aea104208339f1a30ee60a5e48999f09b2967454fd7bf895b7cb15ddd089444a18c67d5fdfe08ba25beaa1a369658956c1a380ef22794828594d48d2'
