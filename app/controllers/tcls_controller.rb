cdd5d19e5d7cbac94d7e1b292a52d49e388b0f77
Jarad Downing <jaraddowning@gmail.com>
2011-05-16 14:29:52 -0400

    Getting ready for NJ

--- app/controllers/tcls_controller.rb
+++ app/controllers/tcls_controller.rb
@@ -6,19 +6,18 @@ class TclsController < ApplicationController
 
   auto_actions_for :event, [:new, :create]
 
+#  def new
+#    hobo_create(Tcl.new(params[:order])) do
+#      hobo_ajax_response if request.xhr?
+#    end
+#  end
 
-  def new
-    hobo_create(Tcl.new(params[:order])) do
-      hobo_ajax_response if request.xhr?
-    end
-  end
-
-  def edit 
-    self.this = User.new(params[:tcl]) if params[:tcl] 
-    hobo_show do 
-      @tcl = this 
-      hobo_ajax_response if request.xhr? 
-    end 
-  end 
+#  def edit 
+#    self.this = User.new(params[:tcl]) if params[:tcl] 
+#    hobo_show do 
+#      @tcl = this 
+#      hobo_ajax_response if request.xhr? 
+#    end 
+#  end 
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-Event.addBehavior({
-    "select.tcl_tcap:change": function(ev) {
-         Hobo.ajaxRequest(window.location.href, ['alevel-part'], {
-             params: Form.serialize(this.up('form')),
-             method: 'get',
-             spinnerNextTo: this,
-             message: 'Please wait...'
-         });
-     }
-});
+//Event.addBehavior({
+//    "select.tcl_tcap:change": function(ev) {
+//         Hobo.ajaxRequest(window.location.href, ['alevel-part'], {
+//             params: Form.serialize(this.up('form')),
+//             method: 'get',
+//             spinnerNextTo: this,
+//             message: 'Please wait...'
+//         });
+//     }
+//});
 
