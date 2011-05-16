7f501630f6bf15ebefdb118ff34f26cdf94003bb
Jarad Downing <jaraddowning@gmail.com>
2011-05-16 14:11:11 -0400

    more breadcrumbs

--- app/controllers/tcls_controller.rb
+++ app/controllers/tcls_controller.rb
@@ -7,18 +7,18 @@ class TclsController < ApplicationController
   auto_actions_for :event, [:new, :create]
 
 
-#  def new
-#    hobo new do
-#      this.attributes = params[:tcl] || {}
-#      hobo_ajax_response if request.xhr?
-#    end
-#  end
+  def new
+    hobo_create(Tcl.new(params[:order])) do
+      hobo_ajax_response if request.xhr?
+    end
+  end
 
-#  def edit
-#    hobo_show do
-#      this.attributes = params[:tcl] || {}
-#      hobo_ajax_response if request.xhr?
-#    end
-#  end
+  def edit 
+    self.this = User.new(params[:tcl]) if params[:tcl] 
+    hobo_show do 
+      @tcl = this 
+      hobo_ajax_response if request.xhr? 
+    end 
+  end 
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//Event.addBehavior({
-//    "form.tcl select.tcl_tcap:change": function(ev) {
-//         Hobo.ajaxRequest(window.location.href, ['level-menu'], {
-//             params: Form.serializeElements([this]),
-//             method: 'get',
-//             spinnerNextTo: this,
-//             message: 'Please wait...'
-//         });
-//     }
-//});
-
-var last_added;
-var last_removed;
-Program.addBehavior({
-  '.funding07_sources:rapid:add' : function(ev) {
-    last_added = ev.memo.element;
-  },
-  '.funding07_sources:rapid:remove' : function(ev) {
-    if(!confirm("really?")) ev.stop();
-  }
+Event.addBehavior({
+    "select.tcl_tcap:change": function(ev) {
+         Hobo.ajaxRequest(window.location.href, ['alevel-part'], {
+             params: Form.serialize(this.up('form')),
+             method: 'get',
+             spinnerNextTo: this,
+             message: 'Please wait...'
+         });
+     }
 });
+
