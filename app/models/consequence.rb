0d046c5fe0d88ab4601766d118c93531396f5fe4
Jarad Downing <jaraddowning@gmail.com>
2011-05-25 14:03:40 -0400

    Permissions on consequences and some js changes

--- app/models/consequence.rb
+++ app/models/consequence.rb
@@ -12,19 +12,19 @@ class Consequence < ActiveRecord::Base
   # --- Permissions --- #
 
   def create_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def update_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def destroy_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def view_permitted?(field)
-    true
+    acting_user.signed_up?
   end
 
 end
