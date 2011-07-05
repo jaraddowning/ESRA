6da6fd0d7c24dde4be2cc9eaacdd5e3041e18660
Jarad Downing <jaraddowning@gmail.com>
2011-07-05 10:54:23 -0400

    csg email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,14 +49,14 @@ Esra::Application.configure do
 
   # Email account setup
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'mail.csg.org',
-    :port => 25,
-    :domain => 'csg.org',
-    :authentication => :login,
-    :user_name => 'emap@csg.org',
-    :password => 'Emap8222'
-  }
+  #config.action_mailer.smtp_settings = {
+  #  :address => 'mail.csg.org',
+  #  :port => 25,
+  #  :domain => 'csg.org',
+  #  :authentication => :login,
+  #  :user_name => 'emap@csg.org',
+  #  :password => 'Emap8222'
+  #}
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
