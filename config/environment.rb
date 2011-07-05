55782d034d2a1a5d299827dc17ef166a3356a7e1
Jarad Downing <jaraddowning@gmail.com>
2011-07-05 10:47:36 -0400

    mail config redux

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Esra::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'emaponline.com',
-  :authentication => :plain,
-  :user_name => 'esraemap@gmail.com',
-  :password => 'EMAP8train'
-}
+#ActionMailer::Base.smtp_settings = {
+#  :address => 'smtp.gmail.com',
+#  :port => 587,
+#  :domain => 'emaponline.com',
+#  :authentication => :plain,
+#  :user_name => 'esraemap@gmail.com',
+#  :password => 'EMAP8train'
+#}
