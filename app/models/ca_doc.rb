8378e695ed0677e84c4927b850807dc1bba225c1
Jarad Downing <jaraddowning@gmail.com>
2011-07-14 08:18:03 -0400

    permissions on ehosts

--- app/models/ca_doc.rb
+++ app/models/ca_doc.rb
@@ -12,15 +12,15 @@ class CaDoc < ActiveRecord::Base
   # --- Permissions --- #
 
   def create_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def update_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def destroy_permitted?
-    acting_user.administrator?
+    acting_user.signed_up?
   end
 
   def view_permitted?(field)
