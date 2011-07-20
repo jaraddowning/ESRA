a34d076f0b9742102ac691a868d35e1e8e116eef
Jarad Downing <jaraddowning@gmail.com>
2011-07-20 13:40:19 -0400

    dryml modifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Esra::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  #config.active_support.deprecation = :notify
+  config.active_support.deprecation = :notify
 
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {
